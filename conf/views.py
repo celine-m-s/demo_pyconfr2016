e5bf17ba5c359c143ba42652d20b91951d99181d
Pierre <pierre@hashbang.fr>
2016-10-15 11:50:59 +0200

    add view index to display all the speakers and their talks

--- conf/views.py
+++ conf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Speaker
+
+
+def index(request):
+    speakers = Speaker.objects.all()
+    return render(request, 'index.html', {'speakers': speakers})
