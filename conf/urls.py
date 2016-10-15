300c1acc51448b7c84fc7ca77f43e50a14e22563
Pierre <pierre@hashbang.fr>
2016-10-15 13:43:52 +0200

    UpdateView for Speaker

--- conf/urls.py
+++ conf/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url
 
-from .views import index, add_speaker, AddSpeakerView
+from .views import index, add_speaker, AddSpeakerView, UpdateSpeakerView
 
 urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^add_speaker/$', add_speaker, name="add_speaker"),
     url(
         r'^add_speaker_cbv/$', AddSpeakerView.as_view(), name="add_speaker_cbv"
+    ),
+    url(
+        r'^update_speaker/(?P<pk>\d+)$',
+        UpdateSpeakerView.as_view(),
+        name="update_speaker"
     ),
 ]
--- conf/views.py
+++ conf/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from .models import Speaker
 from .forms import SpeakerForm
@@ -26,3 +26,10 @@ class AddSpeakerView(CreateView):
     template_name = 'add_speaker.html'
     form_class = SpeakerForm
     success_url = reverse_lazy('index')
+
+
+class UpdateSpeakerView(UpdateView):
+    template_name = 'update_speaker.html'
+    form_class = SpeakerForm
+    success_url = reverse_lazy('index')
+    queryset = Speaker.objects.all()
