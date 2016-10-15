a15314b5b9a28e8fbbdd4270ead7d898a6bf3e82
Pierre <pierre@hashbang.fr>
2016-10-15 13:03:45 +0200

    add another view to create speaker

--- conf/urls.py
+++ conf/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import index
+from .views import index, add_speaker
 
 urlpatterns = [
     url(r'^$', index, name="index"),
+    url(r'^add_speaker/$', add_speaker, name="add_speaker")
 ]
--- conf/views.py
+++ conf/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Speaker
+from .forms import SpeakerForm
 
 
 def index(request):
     speakers = Speaker.objects.all()
     return render(request, 'index.html', {'speakers': speakers})
+
+
+def add_speaker(request):
+    if request.method == 'POST':
+        form = SpeakerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = SpeakerForm()
+    return render(request, 'add_speaker.html', {'form': form})
