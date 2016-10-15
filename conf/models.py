52e7f9472f4b05ff8682c137edadca5a39e399ee
Pierre <pierre@hashbang.fr>
2016-10-15 11:50:16 +0200

    enhance Speaker model

--- conf/models.py
+++ conf/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Speaker(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Talk(models.Model):
     title = models.CharField(max_length=100)
