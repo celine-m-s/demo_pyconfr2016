e09998e00d656ebb9ce905f8489eaabb9874715e
Pierre <pierre@hashbang.fr>
2016-10-15 11:38:03 +0200

    enhance admin

--- conf/admin.py
+++ conf/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from .models import Speaker, Talk
 
 
-admin.site.register(Speaker)
-admin.site.register(Talk)
+class TalkTabularInline(admin.TabularInline):
+    model = Talk
+    extra = 1
+
+
+class SpeakerAdmin(admin.ModelAdmin):
+    model = Speaker
+    inlines = (TalkTabularInline,)
+
+
+admin.site.register(Speaker, SpeakerAdmin)
