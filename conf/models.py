b5a9db877ed159b6efc834c57bfcf97da2c939e0
Pierre <pierre@hashbang.fr>
2016-10-15 11:27:05 +0200

    Start models Speaker and Talk

--- conf/models.py
+++ conf/models.py
@@ -1,5 +1,11 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Talk(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    speaker = models.ForeignKey(Speaker)
