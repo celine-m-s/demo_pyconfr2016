6606eed8844ccebcae109509df59be128246a86d
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2016-10-16 10:16:41 +0200

    Update models.py

--- conf/models.py
+++ conf/models.py
@@ -5,6 +5,7 @@ class Speaker(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
+        # Overcharges to show the name directly in the admin instead of Speaker object.
         return self.name
 
 
